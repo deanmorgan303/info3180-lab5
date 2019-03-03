ebb23804b64c4d728a6a6444e938c4ab069f69b5
dean morgan <dean.morgan@mymona.uwi.edu>
2019-03-03 02:16:20 +0000

    exerize 8

--- app/views.py
+++ app/views.py
@@ -9,7 +9,8 @@ from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
-from app.models import UserProfile
+from app.models import UserProfile 
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -31,20 +32,29 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
-            # Get the username and password values from the form.
-
+            # Get the username and password values from the form. 
+            username = form.username.data 
+            password =form.password.data 
+            
+            user = UserProfile.query.filter(username=username).first() 
+            if user is not None and check_password_hash(user.password,password):
+                remember_me=False  
+                if 'remember_me'in request.form:
+                    remember_me=True
+            
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
 
-            # get user id, load into session
-            login_user(user)
+            # get user id, load into session 
+            
+            login_user(user,remember=remember_me)
 
             # remember to flash a message to the user
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
